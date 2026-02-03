6361b8e0366322c09497f4e1b2948a9ffceeb40b
Raj Yadav <mr.rajyadav272207@gmail.com>
2026-02-03 13:59:44 +0530

    Clean up comments in url.js
    Removed comments for clarity in url.js.

--- routes/url.js
+++ routes/url.js
@@ -7,13 +7,8 @@ const {
 
 const router = express.Router();
 
-// create short url
 router.post("/", handleGenerateShortURL);
-
-// get user urls (dashboard)
 router.get("/user/urls", getUserUrls);
-
-// redirect short url
 router.get("/:shortId", handleRedirect);
 
 module.exports = router;
