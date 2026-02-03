1b869ff22c67a4f6da22d9d79e57cb2afeda76a0
Raj Yadav <mr.rajyadav272207@gmail.com>
2026-02-03 13:29:47 +0530

    Update url.js

--- routes/url.js
+++ routes/url.js
@@ -2,15 +2,15 @@ const express = require("express");
 const {
   handleGenerateShortURL,
   handleRedirect,
-  handleGetUserUrls,
-  handleToggleActive,
+  handleGetAnalytics,
+  getUserUrls,
 } = require("../controllers/url");
 
 const router = express.Router();
 
 router.post("/", handleGenerateShortURL);
-router.get("/user/urls", handleGetUserUrls);
-router.patch("/:id/toggle", handleToggleActive);
+router.get("/user/urls", getUserUrls);
+router.get("/analytics/:shortId", handleGetAnalytics);
 router.get("/:shortId", handleRedirect);
 
 module.exports = router;
